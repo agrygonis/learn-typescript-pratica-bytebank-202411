7e6b5af07b93c8cdc0673ce040013c5bc5a90b60
Adriano Grygonis <agrygonis@gmail.com>
2024-11-15 18:28:50 -0300

    Arrays e type alias

--- dist/js/typescript.js
+++ dist/js/typescript.js
@@ -1,6 +1,10 @@
+// Tipos Primitivos
 let valor = 3000;
 let nome = "";
 let isPago = false;
 let qualquer = "";
 qualquer = 22;
 qualquer = "bola";
+// Arrays
+const lista = [];
+lista.push(11, 12, 22, 13, 13);
--- src/typescript.ts
+++ src/typescript.ts
@@ -1,6 +1,24 @@
+// Tipos Primitivos
 let valor: number = 3000;
 let nome: string = "";
 let isPago: boolean = false;
 let qualquer: any = "";
 qualquer = 22;
 qualquer = "bola";
+
+// Arrays
+const lista: number[] = [];
+lista.push(11, 12, 22, 13, 13);
+
+// Tipos Personalizados
+type Transacao {
+    tipoTransacao: string;
+    data: Date;
+    valor: number;
+}
+
+const novaTransacao: Transacao = {
+    tipoTransacao: "",
+    data: new Date(),
+    valor: 0,
+}
