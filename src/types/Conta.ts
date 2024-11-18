115b0b2938ac2267e2a99891f3bb11ec264473a7
Adriano Grygonis <agrygonis@gmail.com>
2024-11-18 16:03:49 -0300

    Mantendo o registro das transações

--- src/types/Conta.ts
+++ src/types/Conta.ts
@@ -2,6 +2,12 @@ import { Transacao } from "./Transacao.js";
 import { TipoTransacao } from "./TipoTransacao.js";
 
 let saldo: number = 3000;
+const transacoes: Transacao[] = JSON.parse(localStorage.getItem("transacoes"), (key: string, value: string) => {
+    if (key === "data"){
+        return new Date(value);
+    }
+    return value;
+}) || [];
 
 function debitar(valor: number): void{
     if (valor <= 0){
@@ -40,7 +46,9 @@ const Conta = {
             throw new Error("Tipo de Transação é inválido!");
         }
 
+        transacoes.push(novaTransacao);
         console.log(novaTransacao);
+        localStorage.setItem("transacoes", JSON.stringify(transacoes));
     }
 }
 
